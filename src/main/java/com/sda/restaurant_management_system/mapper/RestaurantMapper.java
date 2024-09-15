9a1d2906e63e32e3df9b664f0c1659f5f6662b20
andijaalimerkaj <andija_alimerkaj@yahoo.com>
2024-09-15 13:29:54 +0200

    add RestaurantMapper

--- src/main/java/com/sda/restaurant_management_system/mapper/RestaurantMapper.java
+++ src/main/java/com/sda/restaurant_management_system/mapper/RestaurantMapper.java
@@ -1,4 +1,22 @@
 package com.sda.restaurant_management_system.mapper;
 
+import com.sda.restaurant_management_system.dto.RestaurantDTO;
+import com.sda.restaurant_management_system.model.Restaurant;
+
 public class RestaurantMapper {
+    public static Restaurant mapTo(RestaurantDTO restaurantDTO){
+        Restaurant restaurant= new Restaurant();
+        restaurant.setName(restaurantDTO.getName());
+        return restaurant;
+    }
+    public static RestaurantDTO mapToDTO(Restaurant restaurant){
+        RestaurantDTO restaurantDTO=new RestaurantDTO();
+        restaurantDTO.setName(restaurant.getName());
+        restaurantDTO.setId(restaurant.getId());
+        restaurantDTO.setAddress(restaurant.getAddress());
+        restaurantDTO.setCity(restaurant.getCity());
+        restaurantDTO.setPhoneNumber(restaurant.getPhoneNumber());
+        restaurantDTO.getInstagramUsername(restaurant.getInstagramUsername());
+        return restaurant;
+    }
 }
