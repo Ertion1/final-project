fd6bd48c363fa953175d66ac046c03b47a62404a
fijolard <fijolard9@gmail.com>
2024-09-15 14:02:35 +0200

    dish mapper

--- src/main/java/com/sda/restaurant_management_system/mapper/DishMapper.java
+++ src/main/java/com/sda/restaurant_management_system/mapper/DishMapper.java
@@ -1,4 +1,23 @@
 package com.sda.restaurant_management_system.mapper;
 
+import com.sda.restaurant_management_system.dto.DishDTO;
+import com.sda.restaurant_management_system.model.Dish;
+
+import java.security.PublicKey;
+
 public class DishMapper {
+    public static Dish mapToEntity(DishDTO dishDTO){
+        Dish dish=new Dish();
+        dish.setName(dishDTO.getName());
+        return dish;
+    }
+    public static DishDTO mapToDTO(Dish dishEntity){
+        DishDTO dishDTO = new DishDTO();
+        dishDTO.setId(dishEntity.getId());
+        dishDTO.setName(dishEntity.getName());
+        dishDTO.setPrice(dishEntity.getPrice());
+        dishDTO.setTotalDishCalories(dishEntity.getTotalDishCalories());
+        dishDTO.setRestaurant(RestaurantMapper.mapToDTO(dishEntity.getRestaurant()));
+        return dishDTO;
+    }
 }
