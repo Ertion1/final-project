e914d32b271a0b8ae9e5d7e598254ec7441f2781
Hygerta123 <hygertaferuku@gmail.com>
2024-09-21 13:31:47 +0200

    krijimi i filtering per Orderin

--- src/main/java/com/sda/restaurant_management_system/repository/OrderFilteringRespository.java
+++ src/main/java/com/sda/restaurant_management_system/repository/OrderFilteringRespository.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 @Repository
 @Slf4j
-public class OrderFilteringRepository {
+class OrderFilteringRepository {
 
     @PersistenceContext
     private EntityManager entityManager;
