0a682f672f47d14c7967daa7986dca6ed113f07e
doriname04 <dorinaisaj@gmail.com>
2024-09-22 10:19:37 +0200

    delete

---
+++ src/main/java/com/sda/restaurant_management_system/exceptions/ErrorFormat.java
@@ -0,0 +1,7 @@
+package com.sda.restaurant_management_system.exceptions;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ErrorFormat {
+}
---
+++ src/main/java/com/sda/restaurant_management_system/exceptions/TestExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.sda.restaurant_management_system.exceptions;
+
+import lombok.Data;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Data
+public class TestExceptionHandler {
+}
