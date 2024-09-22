68bae9bff900e308b74154f615686cc7236eb3ec
Ertion <ertionsyziu@gmail.com>
2024-09-22 09:13:24 +0200

    order dish add

--- src/main/java/com/sda/restaurant_management_system/mapper/OrderDishMapper.java
+++ src/main/java/com/sda/restaurant_management_system/mapper/OrderDishMapper.java
@@ -1,5 +1,24 @@
 package com.sda.restaurant_management_system.mapper;
 
-public class OrderDishMapper {
+import com.sda.restaurant_management_system.dto.OrderDishDTO;
+import com.sda.restaurant_management_system.model.Dish;
+import com.sda.restaurant_management_system.model.Order;
+import com.sda.restaurant_management_system.model.OrderDish;
 
+public class OrderDishMapper {
+    public static OrderDish mapToEntity(Order order, Dish dish)
+    {
+        OrderDish orderDish = new OrderDish();
+        orderDish.setDish(dish);
+        orderDish.setOrder(order);
+        return orderDish;
+    }
+    public static OrderDishDTO maoToDTO(OrderDish orderDish)
+    {
+        OrderDishDTO orderDishDTO = new OrderDishDTO();
+        orderDishDTO.setId(orderDish.getId());
+        orderDishDTO.setOrder(OrderMapper.mapToDTO(orderDish.getOrder()));
+        orderDishDTO.setDish(DishMapper.mapToDTO(orderDish.getDish()));
+        return orderDishDTO;
+    }
 }
